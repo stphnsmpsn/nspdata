cb83402429f8563dafc47a14c8371e2558afc99d
Steve Sampson <mail@stephensampson.dev>
2022-10-20 18:35:08 -0300

    Fix accidental commit

--- crates/runner/src/main.rs
+++ crates/runner/src/main.rs
@@ -17,42 +17,5 @@ async fn main() -> Result<(), error::Error> {
         Action::Download => download::download(BIDGELY_BASE_URL, &args).await?,
     }
 
-    let user_id = "your-user-id";
-    let start = 1664593200;
-    let end = 1665413999;
-    let user_auth_response = bidgely_adapter::auth::auth(BIDGELY_BASE_URL, user_id).await?;
-    let session_response =
-        bidgely_adapter::session::session(BIDGELY_BASE_URL, user_auth_response.payload.as_str())
-            .await?;
-    let feed = bidgely_adapter::feed::get_feed(
-        BIDGELY_BASE_URL,
-        user_id,
-        session_response.payload.token_details.access_token.as_str(),
-        start,
-        end,
-    )
-    .await?;
-
-    let interval_blocks: Vec<bidgely_adapter::feed::IntervalBlock> = feed
-        .entry
-        .into_iter()
-        .filter_map(|entry| match entry.content.to_inner() {
-            bidgely_adapter::feed::ContentType::IntervalBlock(e) => Some(e),
-            _ => None,
-        })
-        .collect();
-
-    let mut days: Vec<u32> = vec![];
-
-    interval_blocks.iter().for_each(|interval_block| {
-        let total = interval_block
-            .interval_reading
-            .iter()
-            .fold(0, |acc, x| acc + x.value);
-        days.push(total)
-    });
-
-    days.iter().for_each(|day| println!("{:?}", day));
-
     Ok(())
 }
